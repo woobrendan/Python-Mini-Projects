36ccbcf2a5fdf833353c98543b9bee5aa36066ad
Brendan Woo <woo.brendan@gmail.com>
2022-04-06 16:47:07 -0400

    create folder for guess number game

---
+++ Guess_Number/main.py
@@ -0,0 +1,12 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = input(f'Guess a number between 1 and {x}')
+        print(guess)
+
+
+guess(10)
