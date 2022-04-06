3000c93fe9292ce5839b001e356af3e22b343b5a
Brendan Woo <woo.brendan@gmail.com>
2022-04-06 16:54:17 -0400

    complete logic for user guessed number

--- Guess_Number/main.py
+++ Guess_Number/main.py
@@ -5,8 +5,13 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input(f'Guess a number between 1 and {x}')
-        print(guess)
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry, guess again, too high.')
+    print(
+        f'Yay congrats. You have guessed the number {random_number} correctly')
 
 
 guess(10)
