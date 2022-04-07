1a4840b8a73a5688991f65180394648e05046034
Brendan Woo <woo.brendan@gmail.com>
2022-04-07 14:57:12 -0400

    add edge case checks

--- rock_paper/rps.py
+++ rock_paper/rps.py
@@ -4,8 +4,12 @@ import random
 def play():
     choices = ['r', 's', 'p']
     user = input(
-        "What is your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors :")
+        "What is your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors: ").lower()
     computer = random.choice(choices)
+
+    # Check that user input is a valid rps choice
+    if user not in choices:
+        return "Invalid input"
 
     if user == computer:
         return f'It\'s a tie, You chose {user} and the computer chose {computer}'
@@ -13,7 +17,7 @@ def play():
     if is_win(user, computer):
         return f'You won! You chose {user} and the computer chose {computer}'
 
-    return 'You lost!'
+    return 'You lost! You chose {user} and the computer chose {computer}'
 
 
 def is_win(player, opponent):
