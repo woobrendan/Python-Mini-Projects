94ab9c107a593fc8d1908c711d04d391fff6be47
Brendan Woo <woo.brendan@gmail.com>
2022-04-08 15:49:12 -0400

    add different test

--- Leet_Code/roman_num.js
+++ Leet_Code/roman_num.js
@@ -50,6 +50,48 @@ const romanToInt = (str) => {
       }
     }
   }
+
+//  strArr.forEach((letter, index) => { 
+
+//     if (letter === 'I' &&  strArr[index + 1] === 'V') {
+//       result += 4;
+//       strArr.splice(index , 2);
+//     } 
+//     else if (letter === 'I' &&  strArr[index  + 1] === 'X') {
+//       result += 9;
+//       strArr.splice(index , 2);
+//     } 
+//     else if (letter === 'X' &&  strArr[index  + 1] === 'L') {
+//       result += 40;
+//       strArr.splice(index , 2);
+//       index - 1;
+//     }
+//     else if (letter === 'X' &&  strArr[index  + 1] === 'C') {
+//       result += 90;
+//       strArr.splice(index , 2);
+//       index - 1;
+//     }
+//     else if (letter === 'C' &&  strArr[index  + 1] === 'D') {
+//       result += 400;
+//       strArr.splice(index , 2);
+//       index - 1;
+//     }
+//     else if (letter === 'C' &&  strArr[index  + 1] === 'M') {
+//       result += 900;
+//       strArr.splice(index , 2);
+//       index - 1;
+//     }
+  
+//     else { 
+//       for (const key in romanObj) {
+//         if (key === letter) {
+//           result += romanObj[key];
+//         }
+//       }
+//     }
+//   })
+
+
   return result;
 }
-console.log(romanToInt("MDCCCLXIV"))
+console.log(romanToInt("MDCCXCVII"))
